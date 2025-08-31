c46d5ab640b9ff4bf7ab2907109578b3259bdbc5
ShashankBisht2103 <shashankbisht135@gmail.com>
2025-08-31 10:39:00 +0530

    Update config.py

--- BACKEND/config.py
+++ BACKEND/config.py
@@ -2,7 +2,9 @@ import os
 
 # Your real API key
 OPENWEATHER_API_KEY = "48a9b142fbd636f6efb3213c7f586cad"
-
+OPENWEATHER_API_KEY = "11f58497725da3a88fce6ee433e9782b"
+GENAI_API_KEY = "AIzaSyAojqDJBSH1swhJfxHzNdnW1oJVwiG0_jM"
+OPENAI_API_KEY = "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"
 # OpenWeather endpoints
 OPENWEATHER_CURRENT = "https://api.openweathermap.org/data/2.5/weather"
 OPENWEATHER_FORECAST = "https://api.openweathermap.org/data/2.5/forecast"
